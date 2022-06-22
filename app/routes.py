dcf0c1f43f2167b4a65ac2edd1956dc22ee94954
amantino69 <amantino@yahoo.com>
2022-06-22 13:29:03 -0300

    Meu primeiro commit

---
+++ app/routes.py
@@ -0,0 +1,14 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+def index():
+   
+    nome = "Lopes"
+    dados = {"profissao":"Professora", "Canal":"Profa Alba Lopes"} 
+    return render_template('index.html', nome=nome, dados=dados)
+
+@app.route('/contato')
+def conato():
+    return render_template('contato.html')
