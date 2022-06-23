441c4da260cc316ab9f111cdf8aa7611d57e7260
amantino69 <amantino@yahoo.com>
2022-06-23 15:48:27 -0300

    1

--- microblog.py
+++ microblog.py
@@ -2,6 +2,8 @@ from app import app
 import os
 
 if __name__ == 'main':
-    port = int(os.getenv('PORT'), '5000')
-    app.run('0.0.0.0', port = port)
+    
+    app.run()
+    # port = int(os.getenv('PORT'), '5000')
+    # app.run('0.0.0.0', port = port)
     
